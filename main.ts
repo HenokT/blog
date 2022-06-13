800e68c6c34bbce42d98eb5149aa1ac418e9ddce
Henok Tadesse <htadesse@missiondata.com>
2022-06-13 10:38:36 -0400

    add google analytics

--- main.ts
+++ main.ts
@@ -10,17 +10,14 @@ blog({
     { title: "Email", url: "mailto:hegystyle@gmail.com" },
     { title: "Github", url: "https://github.com/HenokT" },
   ],
+  middlewares: [
+    // If you want to set up Google Analytics, paste your GA key here.
+    ga("G-NRTE0H7K94"),
 
-  // middlewares: [
-
-  // If you want to set up Google Analytics, paste your GA key here.
-  // ga("UA-XXXXXXXX-X"),
-
-  // If you want to provide some redirections, you can specify them here,
-  // pathname specified in a key will redirect to pathname in the value.
-  // redirects({
-  //  "/hello_world.html": "/hello_world",
-  // }),
-
-  // ]
+    // If you want to provide some redirections, you can specify them here,
+    // pathname specified in a key will redirect to pathname in the value.
+    // redirects({
+    //  "/hello_world.html": "/hello_world",
+    // }),
+  ],
 });
